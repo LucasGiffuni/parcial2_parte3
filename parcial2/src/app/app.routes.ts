a177ba5cdd3872c3520c404981daab927f1cb85f
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2023-07-05 19:24:44 -0300

    routes

--- parcial2/src/app/app.routes.ts
+++ parcial2/src/app/app.routes.ts
@@ -5,4 +5,14 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'empresaList',
+	loadComponent: () =>
+	  import('./tab2/empresa-list/empresa-list.component').then((m) => m.EmpresaListComponent)
+  },
+  {
+	path: 'empresa/:id',
+	loadComponent: () =>
+		import('./tab2/empresa-list/empresa/empresa.component').then((m) => m.EmpresaComponent)
+  },
 ];
